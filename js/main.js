66696d4fe68f753c464ef703e2222532ede0db3c
Zakaria Abdisalam <47097630+zabdisalam@users.noreply.github.com>
2023-06-11 23:21:58 -0400

    done

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// function submitForm(event) {
+//     event.preventDefault();
+
+//     const fullName = document.getElementById('fullName').value;
+//     const address = document.getElementById('address').value;
+//     const email = document.getElementById('email').value;
+//     const phone = document.getElementById('phone').value;
+//     const startDate = document.getElementById('startDate').value;
+//     const service = document.getElementById('service').value;
+
+//     const formData = {
+//         fullName,
+//         address,
+//         email,
+//         phone,
+//         startDate,
+//         service
+//     };
+
+//     localStorage.setItem('formData', JSON.stringify(formData));
+
+//     window.location.href = './Success.html'; // replace with your new website URL
+// }
